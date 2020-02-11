fb617350d7717b70b055c4a1d13e2ad37a8c51d2
Sanin <alagic.sanin@nsoft.com>
2020-02-11 13:47:46 +0100

    Add option to pass arg of how many uuids should be generated

--- UuidGenerator.java
+++ UuidGenerator.java
@@ -3,6 +3,23 @@ import java.util.UUID;
 public class UuidGenerator {
 
     public static void main(String[] args) {
-        System.out.println(UUID.randomUUID().toString());
+        int n = 0;
+        int limit = 1;
+        if (args.length == 0) {
+            System.out.println(UUID.randomUUID().toString());
+        }
+        else if(args.length == 1) {
+            try {
+                limit = Integer.parseInt(args[0]);
+            } catch (Throwable e) {
+                System.out.println(e.getMessage());
+            }
+            do {
+                System.out.println(UUID.randomUUID().toString());
+                ++n;
+            } while (n < limit);
+        } else {
+            System.out.println("Invalid usage!\n\nExamples: \nuuid\nuuid 4");
+        }
     }
 }
