f6583e07135e6026e8916d8b0e9bbebddfee9ea5
Sanin <alagic.sanin@nsoft.com>
2020-02-12 09:25:50 +0100

    Fix bash param passing using string literal to keep the param slug

--- autoinstall.sh
+++ autoinstall.sh
@@ -2,5 +2,5 @@ javac UuidGenerator.java
 mkdir -p /usr/local/lib/uuid
 cp UuidGenerator.class /usr/local/lib/uuid
 rm /usr/local/bin/uuid
-echo "java --class-path=/usr/local/lib/uuid UuidGenerator" >> /usr/local/bin/uuid
+echo 'java --class-path=/usr/local/lib/uuid UuidGenerator $1' >> /usr/local/bin/uuid
 chmod a+x /usr/local/bin/uuid
